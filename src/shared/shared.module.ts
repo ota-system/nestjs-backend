d3d751bbd0bd0a3019bc74a1a7e3ff8554da079b
Nguyen <hoangnguyendepgiai@gmail.com>
2026-04-14 16:02:50 +0700

    feat(OTA-20): fix can not run application correctly

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { BullModule } from "@nestjs/bullmq";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { MailerModule } from "@nestjs-modules/mailer";
 import { MailProcessor } from "./mail/mail.processor";
 import { MailService } from "./mail/mail.service";
 import { RedisService } from "./redis/redis.service";
@@ -8,6 +9,7 @@ import { RedisService } from "./redis/redis.service";
 @Module({
 	imports: [
 		ConfigModule,
+		MailerModule,
 		BullModule.registerQueue({
 			name: "mail_queue",
 		}),
